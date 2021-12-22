387c8562667eef7b0401b1314c71872264f13aa7
K Muralidhar Rao <43461932+kmuralidharrao@users.noreply.github.com>
2021-12-22 23:39:56 +0530

    completed the challenges of freecodecamp for node express

--- myApp.js
+++ myApp.js
@@ -1,39 +1,55 @@
 var express = require('express');
+const req = require('express/lib/request');
+require('dotenv').config();
+const bodyParser = require('body-parser');
 var app = express();
 
+console.log("Hello World");
 
+const absolutePath = __dirname + '/views/index.html';
+const absolutePublicPath = __dirname + '/public';
 
+//app.use('/public',express.static(absolutePublicPath));
+//bodyParser.urlencoded({extended: false});
 
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(function (req, res,next) {
+    console.log(req.method +' '+req.path+' - ::'+req.ip);
+    next();
+});
 
+app.use('/public',express.static(absolutePublicPath));
 
+app.get('/',function(req, res) {
+    res.sendFile(absolutePath);
+});
 
+app.get('/json', function (req,res) {
+    console.log("log ",process.env.MESSAGE_STYLE);
+    if (process.env.MESSAGE_STYLE === 'uppercase')
+        res.json({"message": "HELLO JSON"});
+    else
+        res.json({"message": "Hello json"});
+});
 
+app.get('/now',function(req, res, next){
+    req.time = new Date().toString();
+    next();
+}, function(req, res){
+    res.send({time: req.time});
+});
 
+app.get('/:word/echo', function(req,res){
+    res.send({echo: req.params.word})
+});
 
+app.get('/name', function(req,res){
+    res.send({name: req.query.first+' '+req.query.last})
+});
 
+app.post('/name', function(req,res){
+    res.send({name: req.body.first+' '+req.body.last})
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
